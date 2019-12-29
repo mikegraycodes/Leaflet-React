ef0d5b1b66097d7c732931081e8f64e445b6bd36
Michael Gray <michael.gray@synx.com>
2019-12-29 23:15:27 +0000

    Some mapping actions

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -2,12 +2,15 @@ import "./Map.scss";
 import React from "react";
 import L from "leaflet";
 
+const lat = 53.3491415;
+const long = -1.4951546;
+
 class Map extends React.Component {
   componentDidMount() {
     // create map
     this.map = L.map("map", {
       attributionControl: false,
-      center: [53.3491415, -1.4951546],
+      center: [lat, long],
       zoom: 16,
       layers: [L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png")],
       zoomControl: false
@@ -18,6 +21,34 @@ class Map extends React.Component {
         position: "bottomright"
       })
       .addTo(this.map);
+
+    var points = [];
+
+    var marker = L.marker([lat, long]).addTo(this.map);
+    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+
+    var circle = L.circle([lat, long], {
+      color: "red",
+      fillColor: "#f03",
+      fillOpacity: 0.5,
+      radius: 500
+    }).addTo(this.map);
+
+    circle.bindPopup("I am a circle.");
+
+    var polygon = L.polygon([
+      [lat, long],
+      [lat + 0.002, long + 0.0003],
+      [lat - 0.0003, long - 0.00002]
+    ]).addTo(this.map);
+
+    polygon.bindPopup("I am a polygon.");
+
+    function onMapClick(e) {
+      alert("You clicked the map at " + e.latlng);
+    }
+
+    this.map.on("click", onMapClick);
   }
 
   render() {
